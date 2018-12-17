e4c518cc9cce1e690a742b35d696c4362e9289b7
Frank Kluijtmans <frank@studyportals.com>
2018-12-17 19:44:49 +0100

    Testing some stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld :msg="'Welcome to this website!'" />
+    <HelloWorld :msg="'My name is Frank'" />
   </div>
 </template>
 
